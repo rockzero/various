05d00a8a8692739d9e27bf5d20aee16aeddb7390
rockzero <rockzero@try2solve.net>
2013-11-17 12:51:41 +0900

    機能を追加しました。
    +-*/に回数を指定することができます。

--- rpn.rb
+++ rpn.rb
@@ -1,90 +1,149 @@
-# coding: utf-8
-
 $rary = []
 def r_pu
     r = gets
     r.chomp!
     case r
-    when /^(\d)+$/
+    when /^(?:\d)+$/
         n = r.to_i
         $rary.push(n)
         p $rary
         print "\n"
-    when "+"
-    rp
-    when "-"
-    rm
-    when "*"
-    rt
-    when "/"
-    rd
+   when /^((?:\d)*)\+$/
+    rp($1)
+    when /^((?:\d)*)\-$/
+    rm($1)
+    when /^((?:\d)*)\*$/
+    rt($1)
+    when /^((?:\d)*)\/$/
+    rd($1)
     when "q"
     exit
     when "c"
     $rary = []
     p $rary
     puts "\n"
-    #when "s+"
-    #rsp
-    #when "s-"
-    #rsm
-    else
+     else
        puts "数字を入力して下さい。"	
     end
 end
 
-def rp
-    n1 = $rary.pop
-    n2 = $rary.pop
+def rp_seed
+    n1 = $rary.pop if $rary.size != 0
+    n2 = $rary.pop if $rary.size != 0
+    if n1
+    if n2
     n3 = n2 + n1
+    else
+    n3 = n1
+    end
+    elsif n2
+    n3 = n2
+    end
+    if n1 || n2
     $rary.push(n3)
-    puts n3
-    p $rary
-    print "\n"
-end
+    else
+    $rary = []
+    end
+ end
 
-def rm
-    n1 = $rary.pop
-    n2 = $rary.pop
+def rm_seed
+    n1 = $rary.pop if $rary.size != 0
+    n2 = $rary.pop if $rary.size != 0
+    if n1
+    if n2
     n3 = n2 - n1
+    else
+    n3 = n1
+    end
+    elsif n2
+    n3 = n2
+    end
+    if n1 || n2
     $rary.push(n3)
-    puts n3
-    p $rary
-    print "\n"
-end
+    else
+    $rary = []
+    end
+ end
 
-def rt
-    n1 = $rary.pop
-    n2 = $rary.pop
+def rt_seed
+    n1 = $rary.pop if $rary.size != 0
+    n2 = $rary.pop if $rary.size != 0
+    if n1
+    if n2
     n3 = n2 * n1
+    else
+    n3 = n1
+    end
+    elsif n2
+    n3 = n2
+    end
+    if n1 || n2
     $rary.push(n3)
-    puts n3
-    p $rary
-    print "\n"
-end
+    else
+    $rary = []
+    end
+ end
 
-def rd
-    n1 = $rary.pop
-    n2 = $rary.pop
-    n3 = n2 / n1
+def rd_seed
+    n1 = $rary.pop if $rary.size != 0
+    n2 = $rary.pop if $rary.size != 0
+    if n1
+    if n2
+    n3 = Rational(n2, n1)
+    else
+    n3 = n1
+    end
+    elsif n2
+    n3 = n2
+    end
+    if n1 || n2
     $rary.push(n3)
-    puts n3
-    p $rary
-    print "\n"
-end
+    else
+    $rary = []
+    end
+ end
 
-=begin
-def rsp
-    $rary.each{|n1|
-       n2 = 0
-       n2 += n1
-       $rary.delete(n1)
+def rm(n)
+    m = n.empty? ? 1 : n.to_i - 1
+    m.times{
+      rm_seed
     }
-    $rary.push(n2)
-    puts n2
+    puts $rary[-1] if $rary.size > 1
     p $rary
     print "\n"
 end
+
+def rt(n)
+    m = n.empty? ? 1 : n.to_i - 1
+    m.times{
+      rt_seed
+    }
+    puts $rary[-1] if $rary.size > 1
+    p $rary
+    print "\n"
+end
+
+
+def rd(n)
+   m = n.empty? ? 1 : n.to_i - 1
+    m.times{
+      rd_seed
+    }
+    puts $rary[-1] if $rary.size > 1
+    p $rary
+    print "\n"
+end
+
+def rp(n)
+    m = n.empty? ? 1 : n.to_i - 1
+    m.times{
+      rp_seed
+    }
+    puts $rary[-1] if $rary.size > 1
+    p $rary
+    print "\n"
+end
+=begin
 
 def rsm
     n2 = $rary.shift
